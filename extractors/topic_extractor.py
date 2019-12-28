b4d4b4c93a046db967d02bbe7bb8f1847037d4bc
yaser <yaser.jaradeh@gmail.com>
2019-12-28 21:06:27 +0100

    Update Extractor prototype

--- extractors/topic_extractor.py
+++ extractors/topic_extractor.py
@@ -1,10 +1,11 @@
+from typing import List
 from paper_chunk import PaperChunk
 
 
 class TopicExtractor:
 
-    def __init__(self, paper: PaperChunk):
-        self.paper = paper
+    def __init__(self, mag_fields: str = './data/fields.pkl'):
+        self.fields = mag_fields
 
-    def get_topics(self, k: int = 10):
+    def get_topics(self, paper: PaperChunk, k: int = 10) -> List[str]:
         pass
