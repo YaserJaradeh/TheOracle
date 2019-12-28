cf0492f72e2507fa1e097e26ad15ab0d0ce071fb
yaser <yaser.jaradeh@gmail.com>
2019-12-28 20:54:46 +0100

    Add base extractor class

---
+++ extractors/topic_extractor.py
@@ -0,0 +1,10 @@
+from paper_chunk import PaperChunk
+
+
+class TopicExtractor:
+
+    def __init__(self, paper: PaperChunk):
+        self.paper = paper
+
+    def get_topics(self, k: int = 10):
+        pass
