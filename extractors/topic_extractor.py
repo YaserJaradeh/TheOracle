b041611c74f596a96be0725e4ef3b7a569804f31
yaser <yaser.jaradeh@gmail.com>
2019-12-29 18:03:55 +0100

    Add pickle file to base extractor

--- extractors/topic_extractor.py
+++ extractors/topic_extractor.py
@@ -4,8 +4,8 @@ from paper_chunk import PaperChunk
 
 class TopicExtractor:
 
-    def __init__(self, mag_fields: str = './data/fields.pkl'):
-        self.fields = mag_fields
+    def __init__(self, mag_fields: str = '../data/fields.pkl'):
+        self.fields_path = mag_fields
 
     def get_topics(self, paper: PaperChunk, k: int = 10) -> List[str]:
         pass
